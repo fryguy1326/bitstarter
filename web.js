80a874cd0aed95aab6f6c47eefb00fe53e418421
David Freifeld <fryguy1326@gmail.com>
2013-07-30 03:04:14 +0000

    Changed to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buff = fs.readFileSync('index.html');
+  var sOutput = buff.toString('utf-8');
+  response.send(sOutput);
 });
 
 var port = process.env.PORT || 5000;
